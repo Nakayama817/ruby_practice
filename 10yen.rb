c6ed250a546428c512a01c7c2e0e2ef223510720
Nakayama817 <keita.salinger.8316@gmail.com>
2022-10-07 13:54:00 +0900

    first commit

---
+++ 10yen.rb
@@ -0,0 +1,18 @@
+h = 10.0
+i = gets.chomp
+j = i.split
+k = j[0].to_f
+l = j[1].to_f
+m = l - k
+
+if ((k and l) < 0) or ((k and l) > 1000)
+    p "不正な値です"
+    exit
+else
+    if m <= 0
+        p 0
+    else 
+        n = (m/h).ceil
+        p n
+    end
+end
